bbcff84d0a7e2f7fe36eb94c736fcfb202acbcfa
Pedro <pedro-david.lopes@capgemini.com>
2023-03-13 12:27:05 +0000

    Added models relantions

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from .models import Book, Author, Adress, Country
+
 # Register your models here.
+
+
+class BookAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('author', 'rating',)
+    list_display = ('title', 'author',)
+
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Author)
+admin.site.register(Adress)
+admin.site.register(Country)
--- book_outlet/models.py
+++ book_outlet/models.py
@@ -2,7 +2,43 @@ from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.urls import reverse
 from django.utils.text import slugify
+
 # Create your models here.
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=80)
+    code = models.CharField(max_length=2)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Countries'
+
+
+class Adress(models.Model):
+    street = models.CharField(max_length=80)
+    postal_code = models.CharField(max_length=5)
+    city = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.street}, {self.postal_code}, {self.city}'
+
+    class Meta:
+        verbose_name_plural = 'Adress Entries'
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    adress = models.OneToOneField(Adress, on_delete=models.CASCADE, null=True)
+
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def __str__(self):
+        return self.full_name()
 
 
 class Book(models.Model):
@@ -10,13 +46,11 @@ class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)])
-    author = models.CharField(null=True, max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
     is_bestselling = models.BooleanField(default=False)
-    slug = models.SlugField(default='', null=False, db_index=True)
-
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
-        super().save(*args, **kwargs)
+    slug = models.SlugField(default='', blank=True,
+                            null=False, db_index=True)
+    published_countries = models.ManyToManyField(Country)
 
     def get_absolute_url(self):
         return reverse("book-detail", args={self.slug})
